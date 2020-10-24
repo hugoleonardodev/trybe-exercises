5d81928fc3b6c421e62b64e5064e9d887f053c0f
hugoleonardodev <hugoleonardo.dev@gmail.com>
2020-10-24 20:48:25 -0300

    C08 exercise 8

---
+++ introduction-web-development/block_4/day_1/exercise_8.js
@@ -0,0 +1,13 @@
+//True for even false for odd for 3 variables
+
+let a = 25;
+let b = 23;
+let c = 19;
+
+console.log("Are the any even number between a, b and c??");
+
+if (a%2==0 || b%2==0 || c%2==0) {
+    console.log("true");
+}else{
+    console.log("false");
+}
