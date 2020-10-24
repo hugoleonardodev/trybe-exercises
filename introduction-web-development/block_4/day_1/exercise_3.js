4a50311255f86dfea4070c14032c1ba27362725b
hugoleonardodev <hugoleonardo.dev@gmail.com>
2020-10-24 19:19:53 -0300

    C03 add a file js for exercise 3

---
+++ introduction-web-development/block_4/day_1/exercise_3.js
@@ -0,0 +1,21 @@
+// Higher between 3 numbers
+
+let a = 3;
+let b = 5;
+let c = 7;
+
+switch(3) {
+    case 1:
+        (a>b && a>c);
+        console.log(a);
+    break;
+
+    case 2:
+        (b>a && b>c);
+        console.log(b);
+    break;
+
+    case 3:
+        (c>a && c>b);
+        console.log(c);
+}
