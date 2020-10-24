4ce23cea7c85c626eb99fc4a165290fd15427572
hugoleonardodev <hugoleonardo.dev@gmail.com>
2020-10-24 19:51:16 -0300

    C05 add a js file with code for exercise 5

---
+++ introduction-web-development/block_4/day_1/exercise_5.js
@@ -0,0 +1,11 @@
+// Three values of angles == 180 (triangle);
+
+let a = 30;
+let b = 60;
+let c = 60;
+
+if (a+b+c == 180) {
+    console.log("The sum is a triangle.");
+} else {
+    console.log("The sum is not a triangle.");
+}
