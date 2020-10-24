bb6b81e1be14b8aa39b440ee3aa986ff25a246b2
hugoleonardodev <hugoleonardo.dev@gmail.com>
2020-10-24 17:24:27 -0300

    C00 adiciona o arquivo exercise_1

--- introduction-web-development/block_4/day_1/exercise_1.js
+++ introduction-web-development/block_4/day_1/exercise_1.js
@@ -1,1 +1,22 @@
+n1=6;
+n2=2;
 
+let sum = (n1+n2);
+
+console.log(sum);
+
+let diff = (n1-n2);
+
+console.log(diff);
+
+let mult = (n1*n2);
+
+console.log(mult);
+
+let div = (n1/n2);
+
+console.log(div);
+
+let mod = (n1%n2);
+
+console.log(mod);
