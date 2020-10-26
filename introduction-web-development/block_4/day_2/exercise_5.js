6e88134cb0157345ce6858f0ec3795196fe7f766
hugoleonardodev <hugoleonardo.dev@gmail.com>
2020-10-26 18:55:22 -0300

    Create exercise_5.js file

---
+++ introduction-web-development/block_4/day_2/exercise_5.js
@@ -0,0 +1,6 @@
+//Higher element of the Array
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+for (var index=0; index < numbers.length-1; index++) {
+    var elementA = numbers[index];
+    var elementB = numbers[index+1];
+}
