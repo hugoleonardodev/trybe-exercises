0b8ca1d961a9f999db25d35e4370a8146eb56370
hugoleonardodev <hugoleonardo.dev@gmail.com>
2020-10-27 01:50:26 -0300

    Exercise 8 and remove tempcode

--- introduction-web-development/block_4/day_2/tempCodeRunnerFile.js
+++
@@ -1,9 +0,0 @@
-//Higher element of the Array
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let higher = 0;
-for (let index = 0; index < numbers.length; index+=1) {
-    if (numbers[index] > higher) {
-        higher = numbers[index]
-    }
-}
-console.log(`The higher elemet of the array is: ${higher}`);
