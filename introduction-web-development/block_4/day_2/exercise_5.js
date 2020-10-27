710a605eca37172201d906b1a7967419b2d8b891
hugoleonardodev <hugoleonardo.dev@gmail.com>
2020-10-27 01:20:29 -0300

    Exercise 5 higher element of an Array

--- introduction-web-development/block_4/day_2/exercise_5.js
+++ introduction-web-development/block_4/day_2/exercise_5.js
@@ -1,6 +1,9 @@
 //Higher element of the Array
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-for (var index=0; index < numbers.length-1; index++) {
-    var elementA = numbers[index];
-    var elementB = numbers[index+1];
+let higher = 0;
+for (let index = 0; index < numbers.length; index+=1) {
+    if (numbers[index] > higher) {
+        higher = numbers[index]
+    }
 }
+console.log(`The higher elemet of the array is: ${higher}`);
