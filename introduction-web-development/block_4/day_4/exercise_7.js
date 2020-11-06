84e99ab34a722f4f5a88f1c48927af564d16f674
hugoleonardodev <hugoleonardo.dev@gmail.com>
2020-11-06 02:23:25 -0300

    exercise_7.js

---
+++ introduction-web-development/block_4/day_4/exercise_7.js
@@ -0,0 +1,20 @@
+function highestValuePosition(array) {
+    let highestValue;
+    let highestPosition = 0;
+    if (array[0] > array[1]) {
+        highestValue = array[0];
+    } else {
+        highestValue = array[1];
+        highestPosition = 1;
+    }
+    for (let index = 2; index < array.length-1; index += 1) {
+        if (array[index] > highestValue) {
+            highestValue = array[index];
+            highestPosition = index;
+        } else {
+            continue;
+        }
+    }
+    return highestPosition;
+}
+console.log(highestValuePosition([2, 3, 6, 7, 10, 1]));
