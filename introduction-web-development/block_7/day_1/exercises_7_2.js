2fbec0a0d32c13097c6da6bc70b222b40db2e33b
hugoleonardodev <hugoleonardo.dev@gmail.com>
2020-11-24 05:21:46 -0300

    Parte I exercicio 2 coloca o array em ordem crescente usando sort()

--- introduction-web-development/block_7/day_1/exercises_7_2.js
+++ introduction-web-development/block_7/day_1/exercises_7_2.js
@@ -18,6 +18,6 @@ testingScope(false);
 //Exercicio 2
 const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
-// Seu código aqui.
+const sortedArray = (oddsAndEvens) => oddsAndEvens.sort((a, b) => a - b);
 
-console.log(oddsAndEvens);
+console.log(sortedArray(oddsAndEvens));
