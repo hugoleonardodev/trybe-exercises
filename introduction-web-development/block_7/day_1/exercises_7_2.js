d2a6a9ff6cbffb3d360d0d639a8d3dd24885d874
hugoleonardodev <hugoleonardo.dev@gmail.com>
2020-11-24 05:54:32 -0300

    C04 parte I exercicio 2 pronto

--- introduction-web-development/block_7/day_1/exercises_7_2.js
+++ introduction-web-development/block_7/day_1/exercises_7_2.js
@@ -20,4 +20,4 @@ const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
 const sortedArray = (oddsAndEvens) => oddsAndEvens.sort((a, b) => a - b);
 
-console.log(sortedArray(oddsAndEvens));
+console.log(`Os numeros ${sortedArray(oddsAndEvens)} se encontram ordenados de forma crescente!`);
