915d0cc3bc602170851d4bf79c424692331221b0
hugoleonardodev <hugoleonardo.dev@gmail.com>
2020-11-24 21:57:39 -0300

    Parte II exercicio 2 maior palavra de uma frase

--- introduction-web-development/block_7/day_1/exercises_7_2.js
+++ introduction-web-development/block_7/day_1/exercises_7_2.js
@@ -35,3 +35,23 @@ console.log(`Os numeros ${sortedArray(oddsAndEvens)} se encontram ordenados de f
 // fatorial(0);
 const fatorial = (num) => num > 1 ? num * fatorial(num - 1) : num = 1;
 console.log(fatorial(5));
+
+//Exercicio 2
+longestWord("Antônio foi no banheiro e não sabemos o que aconteceu") // retorna 'aconteceu'
+
+const longestWord = (text) => {
+  let textArray = text.split(' ');
+  let maxLength = 0;
+  let result = '';
+  for (let word of textArray) {
+    if (word.length > maxLength) {
+      maxLength = word.length;
+      result = word;
+    }
+  }
+  return result;
+}
+console.log(longestWord("Antonio foi no banheiro e não sabemos o que aconteceu"));
+
+// const longestWord = (texto) => console.log(texto.split(' ')[texto.split(' ').length - 1]);
+// longestWord("Antônio foi no banheiro e não sabemos o que aconteceu"); // retorna 'aconteceu'
