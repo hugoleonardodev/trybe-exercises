6ee2bf9dc49df06e88caf57ea7891f84fadae422
hugoleonardodev <hugoleonardo.dev@gmail.com>
2020-11-26 01:15:48 -0300

    Parte I, Exercicio 2

--- introduction-web-development/block_7/day_2/exercise_7_2.js
+++ introduction-web-development/block_7/day_2/exercise_7_2.js
@@ -34,16 +34,23 @@ const order = {
   },
 };
 
-const customerInfo = (order) => {
-  // Adicione abaixo as informações necessárias.
-  console.log(`Olá Ana Silveira, entrega para: ${order.name}, ${order.phoneNumber}, ${order.address.street}, ${order.address.number}, ${order.address.apartment}.`);
-}
+// const customerInfo = (order) => {
+//   // Adicione abaixo as informações necessárias.
+//   console.log(`Olá Ana Silveira, entrega para: ${order.name}, ${order.phoneNumber}, ${order.address.street}, ${order.address.number}, ${order.address.apartment}.`);
+// }
 
-customerInfo(order);
+// customerInfo(order);
 
 const orderModifier = (order) => {
   // Adicione abaixo as informações necessárias.
-
+  let newName = {name: 'Luiz Silva'};
+  Object.assign(order, newName);
+  let newPaymentTotal = {payment: {total: 50}}
+  Object.assign(order, newPaymentTotal);
+  let drinks = order.drinks;
+  let orderedPizzas = order.order;
+  let flavors = `${Object.keys(orderedPizzas.pizza)[0]}, ${Object.keys(orderedPizzas.pizza)[1]} e ${Object.values(orderedPizzas.drinks.coke)[0]}`;
+  console.log(`Olá ${order.name}, o total do seu pedido de ${flavors} é R$${order.payment.total},00.`);
 }
 
 orderModifier(order);
