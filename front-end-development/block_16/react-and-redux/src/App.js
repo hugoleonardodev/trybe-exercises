1be84c3a190f8d360bfd82a77d39cfe025285c9f
hugoleonardodev <hugoleonardo.dev@gmail.com>
2021-02-12 11:03:42 -0300

    Block 16, day 1 exercises React and Redux

---
+++ front-end-development/block_16/react-and-redux/src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import DisplayMessages from './ReactApp';
+import AppWrapper from './ReactAndRedux';
+
+function App() {
+  return (
+    <div className="App">
+      <DisplayMessages />
+      <AppWrapper />
+    </div>
+  );
+}
+
+export default App;
