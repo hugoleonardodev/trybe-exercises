50d1fe73d6f41d7d6adf546c786fab7b36abdcd7
hugoleonardodev <hugoleonardo.dev@gmail.com>
2021-02-16 22:11:22 -0300

    To Do List with Redux first commit

---
+++ front-end-development/block_16/exercise-todo-list/src/Item.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types'
+
+function Item(props) {
+  const { content, selectTask, value } = props
+  return (
+    <div className="Item" onClick={selectTask} value={value} >
+      {content}
+    </div>
+  );
+}
+
+export default Item;
+
+Item.propTypes = {
+  content: PropTypes.string.isRequired,
+}
