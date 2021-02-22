7758d91087f31a7bfc58ae79e411c1991a7bcb2d
hugoleonardodev <hugoleonardo.dev@gmail.com>
2021-02-22 18:49:12 -0300

    1) Set store Provider on index.js

--- front-end-development/block_16/my-forms-with-react-redux/src/index.js
+++ front-end-development/block_16/my-forms-with-react-redux/src/index.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { Provider } from "react-redux";
+import store from "./store/index";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <Provider store={store}>
     <App />
-  </BrowserRouter>,
-  document.getElementById('root'),
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
